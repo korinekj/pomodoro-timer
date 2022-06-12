691e8f907974316fcf5850bfc2412f45e38e2697
Jarda Korinek <korinek14.j@gmail.com>
2022-06-12 20:11:57 +0200

    pokrok s odečítáním sekund po kliknutí na start button

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -6,6 +6,7 @@ function AppContainer(props) {
 
   const [breakLength, setBreakLength] = useState(initialBreakLength);
   const [sessionLength, setSessionLength] = useState(initialSessionLength);
+  let [countdown, setCountdown] = useState(sessionLength);
 
   /**
    * 4 Funkce na přidávání a ubírání délky trvání přestávky a sezení
@@ -24,13 +25,17 @@ function AppContainer(props) {
 
   const incSession = () => {
     if (sessionLength < 60) {
-      setSessionLength(sessionLength + 1);
+      const newSessionLength = sessionLength + 1;
+      setSessionLength(newSessionLength);
+      setCountdown(newSessionLength);
     }
   };
 
   const decSession = () => {
     if (sessionLength > 1) {
-      setSessionLength(sessionLength - 1);
+      const newSessionLength = sessionLength - 1;
+      setSessionLength(newSessionLength);
+      setCountdown(newSessionLength);
     }
   };
 
@@ -38,14 +43,32 @@ function AppContainer(props) {
    * FUNKCE na resetování do původního stavu (délka přestávky a sezení)
    */
   const resetToDefaultState = () => {
+    const resetSessionLength = 25;
     setBreakLength(initialBreakLength);
-    setSessionLength(initialSessionLength);
+    setSessionLength(resetSessionLength);
+    setCountdown(resetSessionLength);
   };
 
   /**
-   * FUNKCE start timer
+   * FUNKCE ZAČÁTEK ODPOČET
    */
-  const startTimer = () => {};
+  const startTimer = () => {
+    let time = sessionLength * 60;
+
+    const startCountdown = () => {
+      time--;
+      const minutes = Math.floor(time / 60);
+      let seconds = time % 60;
+
+      setCountdown(`${minutes}:${seconds}`);
+
+      if (seconds === 0) {
+        clearInterval(timer);
+      }
+    };
+
+    const timer = window.setInterval(startCountdown, 1000);
+  };
 
   //kód který mi zajistí, že všechny Children AppContaineru budou mít přístup ke stavu! //
   const updateChildrenWithProps = React.Children.map(
@@ -54,6 +77,7 @@ function AppContainer(props) {
       return React.cloneElement(child, {
         break: breakLength,
         session: sessionLength,
+        countdown: countdown,
         setBreak: {
           inc: incBreak,
           dec: decBreak,
--- src/Session.js
+++ src/Session.js
@@ -1,11 +1,13 @@
 function SessionTimer(props) {
   // PŘEVOD z minut NA MM:SS FORMÁT
-  const MMSS = new Date(props.session * 60 * 1000).toString().substring(19, 24);
+  const MMSS = new Date(props.countdown * 60 * 1000)
+    .toString()
+    .substring(19, 24);
 
   return (
     <div className="timer">
       <div id="timer-label">Session</div>
-      <div id="time-left">{MMSS}</div>
+      <div id="time-left">{props.countdown}</div>
       {/* ZDE TO ZPOMALUJE TO TESTOVÁNÍ (FCC RUN TESTS) */}
     </div>
   );
